c7bbb98bfce0348a72b8003152a42d24f00a4e20
Tobi <tobi.wymer@googlemail.com>
2017-09-11 17:05:45 +0200

    Basic Web Site Functionality
     - Added Article Overview
     - Added Article View
     - Added Media Player to Article View

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-/**
- * Created by Tobi on 11.09.2017.
- */
+"use strict";
+var express = require('express');
+var contentHandler = require("./js/ContentHandler");
+contentHandler = new contentHandler();
 
-console.log("Hello World");
+//Init
+var app = express();
+app.set('view engine', 'pug');
+
+
+//Routing
+app.get("/", function(req, res){
+    res.render("index", {
+        articles: contentHandler.getArticles()
+    })
+});
+
+app.get("/articles/:id", function(req, res, next){
+    var articleID = req.params.id;
+    res.render("article", {
+        article: contentHandler.getArticle(articleID)
+    });
+});
+
+app.get("/api/articles/:id", function(req, res, next){
+    var articleID = req.params.id;
+    var article = contentHandler.getArticle(articleID);
+    res.send(article);
+});
+
+app.use(function(req, res){
+    res.send("404 Not Found");
+});
+
+app.listen(1247);
+console.log("Listening on Port 1247");
